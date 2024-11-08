16c264da3cefcf3610f6bf65b5370ceb6b6a90a2
karim <Kroonox88@gmail.com>
2024-11-08 19:01:25 +0100

    00.01.05

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,10 +9,9 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
-    assetsDir: "assets",
     rollupOptions: {
-      output: {
-        manualChunks: undefined,
+      input: {
+        main: path.resolve(__dirname, "index.html"),
       },
     },
   },
